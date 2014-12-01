4d6c801047e95b62b8c61fa71dbda4fa1f86b64c
Bastien Montagne <montagne29@wanadoo.fr>
2014-12-01 19:06:23 +0100

    Squashed commit of mesh-transfer-data branch.

--- release/scripts/startup/bl_ui/space_view3d.py
+++ release/scripts/startup/bl_ui/space_view3d.py
@@ -1095,6 +1095,7 @@ class VIEW3D_MT_object(Menu):
         layout.separator()
 
         layout.operator("object.join")
+        layout.operator("object.data_transfer")
 
         layout.separator()
 
